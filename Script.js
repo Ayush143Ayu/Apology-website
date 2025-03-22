3c9b284ae0ac73fea3fbe1f70e2122f3ee385115
Ayush143Ayu <amorosia8@gmail.com>
2025-03-22 15:10:03 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,4 @@
 
+document.getElementById("apology-btn").addEventListener("click", function() {
+    document.getElementById("message").innerText = "I'm truly sorry, and I hope you can forgive me ❤️";
+});
